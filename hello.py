ebe3f2531351d69f8a8a10256ca761b82eeb2f30
Gaurav Arora <garora2@yahoo.com>
2016-03-09 10:33:05 -0800

    Added callout to execute a.out (executable for the TEA algorithm)

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 import webbrowser
 
 newstr = "Hello"
@@ -7,5 +8,7 @@ print newstr
 urlstr = "http://" +  raw_input('Which URL do you want to go to? ')
 print urlstr
 
+os.system('./a.out > tea.out')
+os.system('cat tea.out')
 webbrowser.open(urlstr)
 
